750c9db9e31196751b1e1d68716e6fea4044d86c
Simao <simao.falcao@my.istec.pt>
2026-08-19 20:15:54 +0100

    Feat/Acc-and-TransactionModels

---
+++ WebApplication1/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    public class Account
+    {       
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty; 
+        public string Type { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+
+        public string Currency { get; set; } = "EUR";
+        ICollection<Transaction> Transactions { get; set; }
+        = new List<Transaction>();
+
+    }
+}
---
+++ WebApplication1/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    //Criei a classe pública designada de Transaction de para definir todos os dados relativos a esta operação.
+    public class Transaction 
+    {
+  
+        public int Id { get; set; }
+
+        //descrição da operação financeira
+        public string Description { get; set; } = string.Empty;
+
+        // Valor da transação, esta em decimal 
+
+        public decimal Amount { get; set; }
+
+        
+        public DateTime Date { get; set; }
+
+
+        // Tipo da transação, por enquanto fica texto (lucro/despesas etc) logo vê-se a possibilidade de usar Enum
+        public string Type { get; set; } = string.Empty;                                                           
+
+        //FK associado
+        public int AccountId { get; set; }
+
+        // Navigation Property -> Transaction -> Property : não esquecer do uso da entity framework
+        // 
+        public Account BankAccount { get; set; } = null!;
+    }
+}
