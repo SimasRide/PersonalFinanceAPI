7e139489225e8f8beb5788aa6cd5d2bb9124900d
Simao <simao.falcao@my.istec.pt>
2026-08-20 15:52:20 +0100

    feat/docker-postgreSQL(config)

---
+++ WebApplication1/Models/Data/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data;
+
+        public class AppDBContext : DbContext
+        {
+            public AppDBContext(DbContextOptions<AppDBContext> options)
+        : base(options){ }
+    public DbSet <Account> Accounts => Set <Account>();
+
+    public DbSet <Transaction> Transactions => Set <Transaction>();
+        }
+    
+
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,15 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
 
-builder.Services.AddRazorPages();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("A connection string da default connection não foi configurada = exceção");
+builder.Services.AddDbContext<AppDBContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+builder.Services.AddRazorPages();
+
+
+
+{
+    app.MapOpenApi();
+}
+
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.UseStaticFiles();
 
