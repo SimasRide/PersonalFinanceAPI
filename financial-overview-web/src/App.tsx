71e3c4cacc0551cb1af0f1719bbfe479307d5ae9
Simao <simao.falcao@my.istec.pt>
2026-09-03 18:25:48 +0100

    feat/App(form)/Main&Index-initialConfig

--- financial-overview-web/src/App.tsx
+++ financial-overview-web/src/App.tsx
@@ -1,2 +1,71 @@
-imort {useState} from 'react';
+// useState guarda valores que podem mudar durante a utilização da página.
+// "type FormEvent" importa apenas o tipo do evento.
+import { useState, type FormEvent } from "react";
+
 import "./App.css";
+
+function App() {
+
+  const [loginEmail, setLoginEmail] = useState("");
+
+ 
+  const [loginPassword, setLoginPassword] = useState("");
+
+  /*Envio  a partir de formHTML*/
+  function handleLogin(event: FormEvent<HTMLFormElement>) {
+    // Impede o recarregamento da página.
+    event.preventDefault();
+
+    // Apagar depois 
+    console.log({
+      email: loginEmail,
+      password: loginPassword,
+    });
+  }
+
+  return (
+    // Tudo o que pertence ao formulário fica dentro desta tag.
+    <form onSubmit={handleLogin}>
+
+      {/* EMAIL */}
+      <label htmlFor="login-email">
+        Email:
+      </label>
+
+      <input
+        id="login-email"
+        name="email"
+        type="email"
+        value={loginEmail}
+        onChange={(event) =>
+          setLoginEmail(event.target.value)
+        }
+        required
+      />
+
+      {/*PASSWORD*/}
+
+      <label htmlFor="login-password">
+        Password:
+      </label>
+
+      <input
+        id="login-password"
+        name="password"
+        type="password"
+        value={loginPassword}
+        onChange={(event) =>
+          setLoginPassword(event.target.value)
+        }
+        required
+      />
+
+      {/*  Sem necessidade de repetir onSubmit= visto que ja está no form acima */}
+      <button type="submit">
+        Log In
+      </button>
+    </form>
+  );
+}
+
+export default App;
